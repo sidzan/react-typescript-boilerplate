8d306a731c8b108aefe82bee942f438e64c1703d
Sijan <sijanshrestha2@gmail.com>
2021-03-09 14:20:35 +0700

    feat: add button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import {Button as MaterialButton} from '@material-ui/core';
+
+const Button = () => (<MaterialButton color="primary">Hello World</MaterialButton>);
+
+export default Button;
