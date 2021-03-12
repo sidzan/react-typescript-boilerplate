ea98cf2c64b3e901e2ee9c9438b200e0305fb6db
sijan <sijanshrestha2@gmail.com>
2021-03-12 14:27:34 +0700

    feat: add router and configs (#5)

---
+++ src/Layout/DefaultLayout.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import {renderRoutes} from "react-router-config";
+import {Link} from "react-router-dom";
+
+// @ts-ignore
+const DefaultLayout: React.FC = ({route}) => (
+    <div>
+        <h1>Root</h1>
+        <ul style={{display: 'flex', justifyContent: 'space-around'}}>
+            <li><Link to={"/"}>Home</Link></li>
+            <li><Link to={"/about"}>About</Link></li>
+            <li><Link to={"/stars"}>Stars</Link></li>
+            <li><Link to={"/contact"}>Contact</Link></li>
+        </ul>
+        {/* child routes won't render without this */}
+        <React.Suspense fallback={<div>Loading...</div>}>
+            {renderRoutes(route.routes)}
+        </React.Suspense>
+    </div>
+);
+
+export default DefaultLayout
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+
+import {createBrowserHistory} from 'history';
+
+export const history = createBrowserHistory();
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
